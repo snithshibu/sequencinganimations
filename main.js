528422a2c990b848497a1bf40584ffd4cb0e22f9
Snith SHibu <snithshibu.b23ec1156@mbcet.ac.in>
2024-12-17 23:55:23 +0300

    changes

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const aliceTumbling = [
-    { transform: 'rotate(0) scale(1)' },
-    { transform: 'rotate(360deg) scale(0)' }
-  ];
-  
-  const aliceTiming = {
-    duration: 2000,
-    iterations: 1,
-    fill: 'forwards'
-  }
-  
-  const alice1 = document.querySelector("#alice1");
-  const alice2 = document.querySelector("#alice2");
-  const alice3 = document.querySelector("#alice3");
+  { transform: "rotate(0) scale(1)" },
+  { transform: "rotate(360deg) scale(0)" },
+];
 
-  alice1.animate(aliceTumbling, aliceTiming);
+const aliceTiming = {
+  duration: 2000,
+  iterations: 1,
+  fill: "forwards",
+};
+
+const alice1 = document.querySelector("#alice1");
+const alice2 = document.querySelector("#alice2");
+const alice3 = document.querySelector("#alice3");
+
+const first = alice1.animate(aliceTumbling, aliceTiming);
+
+first.finished.then(() => {
+  const second = alice2.animate(aliceTumbling, aliceTiming);
+
+  second.finished.then(() => {
+    alice3.animate(aliceTumbling, aliceTiming);
+  });
+});
